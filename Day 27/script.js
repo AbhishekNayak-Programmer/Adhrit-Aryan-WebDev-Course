78ee7449bb16291a5c163b5c0a70a15c25bd83b6
Abhisheknayak-programer <sahiservice300@gmail.com>
2025-10-05 11:36:11 +0530

    Session 27 Done

---
+++ Day 27/script.js
@@ -0,0 +1,18 @@
+console.log("Abhishek Nayak");
+console.log((79 * 23467 * 3467) / 2345);
+
+// + - * / %
+// 9 % 2 = 1
+// `
+
+console.log(`Total bill amount ${(79 * 23467 * 3467) / 2345}`);
+
+// Homework
+// 1. add a google map of your favorite place
+// 2. add your favorite youtuber video
+// 3. Download a video from pexels and then attach it to web page with autoplay functionality
+// 4. Create a table for product_id, product_name, product_desc, product_price
+// 5. Download a music and make it autoplay in the website
+// 6. Create a animation of square to become diffrent shapes likes circle, rectangle, rhombous
+// 7. Create a form like this - link - https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.canva.com%2Fdocuments%2Ftemplates%2Fform%2F&psig=AOvVaw3DMRAHc5Eky3YeSTbzZBiw&ust=1759729814944000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCKCbw8eujJADFQAAAAAdAAAAABAQ
+// 8. Go through all the codes from day 1 to day 27
