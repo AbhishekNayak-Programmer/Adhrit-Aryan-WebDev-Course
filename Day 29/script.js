7774589bc9e249550f968418bd297b2cbc3d7de3
Abhisheknayak-programer <sahiservice300@gmail.com>
2025-10-19 11:22:35 +0530

    Session 29 done

---
+++ Day 29/script.js
@@ -0,0 +1,38 @@
+var blood_group = "O+ve";
+let age = 18;
+const name = "Abhishek";
+
+age = 40;
+
+// name = "Sahil";
+
+console.log(age);
+console.log(name);
+
+blood_group = "A+ve";
+console.log(blood_group);
+
+var abc;
+console.log(abc);
+
+const abcd = null;
+console.log(abcd);
+
+let person = {
+  name: "Abhishek",
+  age: 26,
+  height: "6ft",
+  location: "hyderabad",
+  blood_group: "O+ve",
+};
+
+person.age = 27;
+person.location = "Bengaluru";
+
+console.log(person);
+
+let price = 12.345839456;
+console.log(Math.trunc(price));
+console.log(Math.floor(price));
+console.log(Math.ceil(price));
+console.log(Math.PI);
